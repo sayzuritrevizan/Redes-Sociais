b31671783615c82eea2cea9298c571a07deb892f
sayzuritrevizan <148063702+sayzuritrevizan@users.noreply.github.com>
2024-10-16 10:08:00 -0300

    Update informacoesGlobais.js

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -1,1 +1,9 @@
+const url = ""
 
+async function visualizarInformacoesGlobais() {
+  const res = await fetch(url)
+  const dados = await res.json()
+  console.log(dados);
+}
+
+visualizarInformacoesGlobais()
