6f8665aba326494f5039d953a9945593b944a4f2
sayzuritrevizan <148063702+sayzuritrevizan@users.noreply.github.com>
2024-10-23 10:23:00 -0300

    Update informacoesGlobais.js

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -3,7 +3,13 @@ const url = "https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-g
 async function visualizarInformacoesGlobais() {
   const res = await fetch(url)
   const dados = await res.json()
-  console.log(dados);
+  const pessoasConectadas = (dados.total_pessoas_conectadas /1e9)
+  const pessoasNoMundo = (dados.total_pessoas_mundo /1e9)
+  const horas = parseInt(dados.tempo_medio)
+  const minutos = Math.round((dados.tempo_medio - horas) * 100)
+  const contagemCinectada = ((pessoasConectadas / pesoasNoMundo) *100).toFixed(2)
+
+  const paragrafo = document.createElement('p')
 }
 
 visualizarInformacoesGlobais()
